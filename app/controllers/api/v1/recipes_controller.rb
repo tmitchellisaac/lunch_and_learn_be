1bc6ea60e4c585c02c5f9d03bcc513cdf81209b1
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-02 13:49:52 -0700

    Add recipes controller with index action that serializes a recipe poro after calling edamam api for the queried country

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::RecipesController < ApplicationController
+
+  def index
+    facade = RecipeFacade.new(params).recipes
+    render json: RecipeSerializer.new(facade).serializable_hash.to_json
+  end
+
+
+end
