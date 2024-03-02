64c239fb2501105c7db3b3c806c08ceb6c90ec64
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-02 15:45:18 -0700

    Add a country service with get country method calling restcountries api

---
+++ app/services/country_service.rb
@@ -0,0 +1,19 @@
+class CountryService 
+
+  def conn
+    Faraday.new(url: "https://restcountries.com/v3.1") do |faraday|
+    end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_country
+    result = get_url("all?fields=name")
+  end
+
+
+
+end
