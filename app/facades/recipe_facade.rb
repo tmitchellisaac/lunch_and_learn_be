b7b411c59096228929654e3c29b1d4ccbbdf6490
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-02 15:44:57 -0700

    Add some logic for random country to recipes method in recipe facade

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -5,13 +5,21 @@ class RecipeFacade
     @country = params[:country]
   end
 
-
-
   def recipes
     service = RecipeService.new
-    r = service.get_recipes(@country)
-    x = r.map do |recipe|
-      Recipe.new(recipe, @country)
+    if @country && @country != ""
+      service.get_recipes(@country).map do |recipe|
+        Recipe.new(recipe, @country)
+      end
+    else
+      country_name_array = []
+      CountryService.new.get_country.map do |country_data|
+        country_name_array << country_data[:name][:common]
+      end
+      rando_country = country_name_array.sample
+      service.get_recipes(rando_country).map do |recipe|
+        Recipe.new(recipe, rando_country)
+      end
     end
   end
 end
