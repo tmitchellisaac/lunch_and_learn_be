82c76c53a09b83edca992802c73573d81a647c18
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-02 19:02:23 -0700

    Create LearningVideoService to make youtube video service calls

---
+++ app/services/learning_video_service.rb
@@ -0,0 +1,26 @@
+class LearningVideoService
+
+  def conn
+    Faraday.new(url: "https://youtube.googleapis.com/youtube/v3/search") do |faraday|
+    faraday.params["key"] = Rails.application.credentials.YOUTUBE[:KEY]
+    faraday.params["channelId"] = "UCluQ5yInbeAkkeCndNnUhpw" #Mr. History account
+    faraday.params["part"] = "snippet"
+    faraday.params["maxResults"] = "1"
+    end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_learning_video(country)
+    get_url("?q=#{country}")
+  end
+
+  # def get_recipes(country)
+  #   get_url("?q=#{country}")[:hits]
+  # end
+
+
+end
