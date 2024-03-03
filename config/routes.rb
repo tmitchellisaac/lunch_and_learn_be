7d300c0e09da77513ff128c7928636c02f4279cc
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-02 19:03:01 -0700

    Add learning_resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
+        resources :learning_resources
         resources :recipes
       end
     end
