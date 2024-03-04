49816e3792093cd9a344844787982e8d49009fea
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-04 10:51:48 -0700

    Add airquality routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
+        resources :air_quality, only: :index
         post "/sessions", to: "sessions#login"
         resources :users, only: :create
         resources :learning_resources, only: :index
