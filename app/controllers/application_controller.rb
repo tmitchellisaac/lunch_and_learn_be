ad7ca0178163aaf3ced3609f9731ee7477d5b07a
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-03 18:24:51 -0700

    Add some error handling to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::ParameterMissing, with: :param_missing
+  rescue_from ActiveRecord::RecordInvalid, with: :uniq_email
+
+  # def param_missing
+  #   render json: ErrorSerializer.new(Error.new(exception.message))
+  #       .serialize_json, status: 400
+  # end
+
+  def uniq_email
+    render json: ErrorSerializer.new(Error.new("email is not unique", 400)), status: 400
+   end
 end
