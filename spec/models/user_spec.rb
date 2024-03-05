ae4b7784e1bf24af666363b6ba52539e04748fcd
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-04 17:16:06 -0700

    Add favorites association to user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-describe User, type: :model do
+RSpec.describe User, type: :model do
   describe "validations" do
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
     it {should validate_presence_of(:api_key)}
+    it {should have_many(:favorites)}
 
   end
 end
