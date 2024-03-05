9bf922d2cae206bc30b4e515bc6e541af9e85b0f
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-04 17:15:03 -0700

    Add favorites table with a users foreign key, country, recipe_title, and recipe_link

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_03_204431) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_04_214018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "country"
+    t.string "recipe_link"
+    t.string "recipe_title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_204431) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "users"
 end
