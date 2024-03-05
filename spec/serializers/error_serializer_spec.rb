34c89caa6f68b168c5ac8e5c5109ca815a8e6cab
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-05 13:56:12 -0700

    Add some tests to error serializer

--- spec/serializers/error_serializer_spec.rb
+++ spec/serializers/error_serializer_spec.rb
@@ -5,5 +5,9 @@ describe "Error Serializer" do
     poro = Error.new("incorrect API key", 401)
     serializer = ErrorSerializer.new(poro)
     expect(serializer).to be_a(ErrorSerializer)
+    expect(serializer.error).to be_a(Error)
+    expect(serializer.error.message).to eq("incorrect API key")
+    expect(serializer.error.status_code).to eq(401)
+
   end
 end
