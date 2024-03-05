52a31005429693b3ad76dca1354e0891c5954410
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-04 17:15:14 -0700

    Add some users and favorites to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+user_1 = User.create!(name: "Todd", email: "cheese@gmail.com", password: 'password1', api_key: "qwertyuioplkjhgfdsazxcvb")
+favorite_1 = Favorite.create!(user_id: user_1.id, country: 'thailand', recipe_link: "https://www.tastingtable.com/.....", recipe_title: "You're Telling Me A Crab Fried This Rice??" )
+favorite_2 = Favorite.create!(user_id: user_1.id, country: 'greece', recipe_link: "https://www.tastingtable.com/.....", recipe_title: "Spanikopita" )
+favorite_2 = Favorite.create!(user_id: user_1.id, country: 'india', recipe_link: "https://www.tastingtable.com/.....", recipe_title: "Tandoori Chicken" )
+
+user_2 = User.create!(name: "Wanda", email: "grapes@gmail.com", password: 'password1', api_key: "bvcxzasdfghjklpoiuytrewq")
